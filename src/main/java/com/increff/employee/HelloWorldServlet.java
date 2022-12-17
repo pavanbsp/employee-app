272491a0242ad4f20917aa7524a2750e83e89f0a
Pavan <pavanboina6@gmail.com>
2022-12-17 22:12:19 +0530

    initial

---
+++ src/main/java/com/increff/employee/HelloWorldServlet.java
@@ -0,0 +1,35 @@
+package com.increff.employee;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorld", description = "sample servlet", urlPatterns = { "/hello-world" })
+public class HelloWorldServlet extends HttpServlet {
+    private static final long serialVersionUID = -2934514256330525834L;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<p>Hello Increff!</p>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
